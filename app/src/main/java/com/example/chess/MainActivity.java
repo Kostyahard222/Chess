06f6492d61a38b903f1d7bd35f4ca7c1742c571c
Kostyahard222 <Kostyahard222@ya.ru>
2022-05-30 15:03:56 +0300

    рщшг

--- app/src/main/java/com/example/chess/MainActivity.java
+++ app/src/main/java/com/example/chess/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.chess;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,18 +14,93 @@ import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+class figure{
+    int X,Y,id; //хз зачем х и у пусть пока будут
+    //id работают
+    //1 символ w (white) белые или b (black) черные
+    //2 символ r (rook) ладья h (horse) конь b (bishop)
+    //слон q (queen) ферзь k (king) король p (pawn) пешка (навального)
+    //3 символ чтобы была уникальность а именно буква столбца в котором изначально стоит фигура
+    public figure(int x, int y){
+        X=x;
+        Y=y;
+    }
+    public void availablemoves(){}
+}
+class wpawn extends figure{ //пешка
+    public wpawn(int x, int y, int id){
+        super(x,y);
+        id = 1;
+    }
+    @Override
+    public void availablemoves() {
+        
+    }
+}
+class bpawn extends figure{ //пешка
+    public bpawn(int x, int y, int id){
+        super(x,y);
+        id = 1;
+    }
+    @Override
+    public void availablemoves() {
+
+    }
+}
+class wrook extends figure{ //пешка
+    public wrook(int x, int y, int id){
+        super(x,y);
+        id = 1;
+    }
+    @Override
+    public void availablemoves() {
+
+    }
+}
+class brook extends figure{ //пешка
+    public brook(int x, int y, int id){
+        super(x,y);
+        id = 1;
+    }
+    @Override
+    public void availablemoves() {
+
+    }
+}
+
+public class MainActivity extends AppCompatActivity {
+    static String[][] grid =
+            {
+                    {"wr", "wh", "wb", "wq", "wk", "wb", "wh", "wr"},
+                    {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
+                    {"00", "00", "00", "00", "00", "00", "00", "00"},
+                    {"00", "00", "00", "00", "00", "00", "00", "00"},
+                    {"00", "00", "00", "00", "00", "00", "00", "00"},
+                    {"00", "00", "00", "00", "00", "00", "00", "00"},
+                    {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
+                    {"br", "bh", "bb", "bq", "bk", "bb", "bh", "br"}
+            };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        MyDraw z = new MyDraw(getApplicationContext()); // z не потому что Россия, а потому что первое пришло в голову
+        setContentView(z); // я в душе не ебу что я тут сделал, но кажется оно работает (нет)
+    }
+}
 class MyDraw extends View {
     static Paint paint = new Paint();
-    static Canvas ss; // это сделано что бы выдрать входящий канвас и дальше его использовать
+    int center = getHeight() / 2, tilewidth = (getWidth() - 30) / 8;
     public MyDraw(Context context) {
         super(context);
     }
-    int center = getHeight() / 2, tilewidth = (getWidth() - 30) / 8;
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        ss = canvas; // вот от сюда и выдран канвас
         paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(55.0f);
         paint.setARGB(255, 157, 224, 180); //задний фон изумрудного цвета
         canvas.drawPaint(paint);
         paint.setAntiAlias(true);
@@ -64,64 +141,99 @@ class MyDraw extends View {
         canvas.drawRect(15 + tilewidth * 3, center - (tilewidth * (-3)), 15 + tilewidth * 3 + tilewidth, center - (tilewidth * (-3)) + tilewidth, paint);
         canvas.drawRect(15 + tilewidth * 5, center - (tilewidth * (-3)), 15 + tilewidth * 5 + tilewidth, center - (tilewidth * (-3)) + tilewidth, paint);
         canvas.drawRect(15 + tilewidth * 7, center - (tilewidth * (-3)), 15 + tilewidth * 7 + tilewidth, center - (tilewidth * (-3)) + tilewidth, paint);
-    }
-    public void GridUpdate(int[][] o) {
-        paint.setAntiAlias(true);
-        paint.setTextSize(55.0f);
-        paint.setColor(Color.WHITE);
-        float center = getHeight() / 2;
-        float tilewidth = (getWidth() - 30) / 8;
-        float startingtop = center - (tilewidth * 4);
-        for (int i = 0;i<8;i++){
-            for (int j = 0;j<8;j++){
-        ss.drawText("" + o[7-i][j],center-(tilewidth*4)+(tilewidth*i)+tilewidth/2,15+(tilewidth*j)+tilewidth/2,paint); // эта строка вызывает ошибку с нулевым референсом
-                // жаль что не жожо референсом
+        paint.setColor(Color.argb(255,255,0,0));
+        Bitmap b;
+        int zc = (tilewidth - 60)/2;
+        for (int i = 0; i<8; i=i+1){
+            for (int j = 0; j<8; j=j+1){
+                int YY = center - (tilewidth * 4) + (tilewidth * i);
+                switch (MainActivity.grid[7-i][j]){
+                    case "wp":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.wp);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "wr":
+                         b = BitmapFactory.decodeResource(getResources(), R.drawable.wr);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "wh":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.wh);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "wb":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.wb);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "wq":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.wq);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "wk":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.wk);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "bp":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.bp);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "br":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.br);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "bh":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.bh);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "bb":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.bb);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "bq":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.bq);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j),
+                                YY,
+                                paint);
+                        break;
+                    case "bk":
+                        b = BitmapFactory.decodeResource(getResources(), R.drawable.bk);
+                        canvas.drawBitmap(b,
+                                15+(tilewidth*j) ,
+                                YY,
+                                paint);
+                        break;
+                }
             }
         }
-    }
-
-
-}
-class figure{
-    int X,Y,id; //хз зачем х и у пусть пока будут
-    //id работают с 1 по 6 у белых фигур с 7 по 12 у черных
-    //1 и 7 это пешка 2 и 8 это король 3 и 9 это ферзь
-    //4 и 10 это ладья 5 и 11 это слон 6 и 12 это конь
-    public figure(int x, int y){
-        X=x;
-        Y=y;
-    }
-    public void availablemoves(){}
-}
-class wpawn extends figure{ //белая пешка
-    public wpawn(int x, int y, int id){
-        super(x,y);
-        id = 1;
-    }
-    @Override
-    public void availablemoves() {
-    }
-}
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        int[][] grid =
-                {
-                        {4, 6, 5, 3, 2, 5, 6, 4},
-                        {1, 1, 1, 1, 1, 1, 1, 1},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0},
-                        {7, 7, 7, 7, 7, 7, 7, 7},
-                        {10, 12, 11, 9, 8, 11, 12, 10}
-                };
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        MyDraw z = new MyDraw(getApplicationContext()); // z не потому что Россия, а потому что первое пришло в голову
-        setContentView(z); // я в душе не ебу что я тут сделал, но кажется оно работает (нет)
-        z.GridUpdate(grid);
     }
 }
